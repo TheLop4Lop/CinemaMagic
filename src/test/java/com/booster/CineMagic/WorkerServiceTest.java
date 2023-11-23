02f9b66ee577fffc9a0038c6fb602a1013860168
PedroMtz <thelop4lop@gmail.com>
2023-11-22 19:48:04 -0600

    Adding Mock and InjectMockito

--- src/test/java/com/booster/CineMagic/WorkerServiceTest.java
+++ src/test/java/com/booster/CineMagic/WorkerServiceTest.java
@@ -1,8 +1,17 @@
 package com.booster.CineMagic;
 
+import com.booster.CineMagic.Repository.IWorkerRepository;
+import com.booster.CineMagic.Service.WorkerService;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 public class WorkerServiceTest {
+
+    @Mock
+    IWorkerRepository workerRepository;
+    @InjectMocks
+    WorkerService workerService;
+
 
 
 }
